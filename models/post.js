e50f138e06918e781357e4d2fbaf9274eb6f393e
JohnWestermeyer <johnwestermeyer@gmail.com>
2021-01-17 15:30:39 -0800

    more database and server stuff

---
+++ models/post.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+    const Post = sequelize.define("Post", {
+    //code block (string max)
+    //user id (string)
+    //tags 
+    //post id (sequleize)
+    });
+    return Post;
+}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 var db = require("./models");
+const passport = require("./config/passport");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,16 +13,24 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+//passport
+app.use(
+    session({ secret: "placeholder", resave: true, saveUninitialized: true })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 // Define API routes here
 require("./routes/apiroutes.js")(app);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  });
+  
