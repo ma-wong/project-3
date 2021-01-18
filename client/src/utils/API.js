e5fb6c83af157e9bea57aea7b7864613737838b6
Lawson Kelly <lawsonkelly@Lawsons-MacBook-Air.local>
2021-01-18 17:23:42 -0500

    password max

---
+++ client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=1";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default {
+  getRandomUserImage: function() {
+    return axios.get(BASEURL);
+  }
+};
