4381c2ce3b5c270c3436b03a74aa742e358c15a7
Brandon Piercy <b.piercy567@gmail.com>
2021-01-18 13:47:03 -0800

    made basic post controller routes

---
+++ contollers/postController.js
@@ -0,0 +1,37 @@
+const db = require("../models/post.js");
+
+module.exports = {
+    create: function(req, res) {
+        db.Post.create({
+            title: req.body.title,
+            code: req.body.code,
+            userid: req.body.userid,
+            tags: req.body.tags
+        })
+        .then((dbPost) => {res.json(dbPost)})
+        .catch(err => {throw err});
+    },
+    readAll: function(req, res) {
+        db.Post.findAll({})
+        .then((dbPost) => {res.json(dbPost)})
+        .catch( err => {throw err});
+    },
+    // Parameters for findone, delete and update may need to be adjusted to something other than ID idk;
+    update: function(req, res) {
+        db.Post.update({
+            email: req.body.email,
+            password: req.body.password,
+            Postname: req.body.Postname
+        }, { where: { id: req.body.id }})
+        .then((dbPost) => {res.json(dbPost)})
+        .catch( err => {throw err});
+    },
+    delete: function(req, res) {
+        db.Post.destroy({
+            where: { id: req.params.id }
+        })
+        .then((dbPost) => {res.json(dbPost)})
+        .catch( err => {throw err});
+    }
+    // Still needs search by title and tag routes
+};
