ebc14cb0dc6f3f7e2584252c2cb04ab6b9c5c6f3
Lawson Kelly <lawsonkelly@Lawsons-MacBook-Air.local>
2021-01-19 17:00:15 -0500

    login good

---
+++ client/src/components/SignUpForm/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function SignUpForm({ username, email, password, confirmPassword, handleInputChange, handleFormSubmit }) {
+
+  return (
+ <div className="container">
+    <div className="row">
+      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div className="card card-signin my-5">
+          <div className="card-body">
+            <h5 className="card-title text-center">Sign Up</h5>
+            <form className="form-signin">
+              <div className="form-label-group">
+                <input
+                 onChange={handleInputChange} 
+                 type="input" 
+                 id="inputEmail" 
+                 className="form-control" 
+                 placeholder="Email address" 
+                 //react stuff
+                 name="email"
+                 value={email}
+                 required autoFocus />
+                <label htmlFor="inputEmail">Email address</label>
+              </div>
+
+              <div className="form-label-group">
+                <input 
+                onChange={handleInputChange} 
+                type="input" 
+                id="inputUsername" 
+                className="form-control" 
+                placeholder="Username" 
+                //react stuff
+                 name="username"
+                 value={username}
+                required />
+                <label htmlFor="inputUsername">Username</label>
+              </div>
+
+              <div className="form-label-group">
+                <input onChange={handleInputChange} 
+                type="password" 
+                id="inputPassword" 
+                className="form-control" 
+                placeholder="Password" 
+                //react stuff
+                name="password"
+                value={password}
+                required />
+                <label htmlFor="inputPassword">Password</label>
+              </div>
+
+              {/* <div className="form-label-group">
+                <input onChange={handleInputChange} 
+                type="password" 
+                id="inputConfirmPassword" 
+                className="form-control" 
+                placeholder="Confirm Password" 
+                name="confirmPassword"
+                value={confirmPassword}
+                required />
+                <label htmlFor="inputConfirmPassword">Confirm Password</label>
+              </div> */}
+              <div className="custom-control custom-checkbox mb-3">
+                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                <label className="custom-control-label" htmlFor="customCheck1">Agree to Terms and Conditions</label>
+              </div>
+              <button onClick={handleFormSubmit} className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign Up</button>
+              <hr />
+              <div className="terms">
+                    <a href="/">Terms of Service</a>
+                    <a href="/">Privacy</a>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  );
+}
+
+export default SignUpForm;
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Mailer } from 'nodemailer-react';
+import SignUpForm from "../components/SignUpForm";
+function Signup() {
+    const [userState, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+//put this into it's own file but for now do this
+
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setUser({
+            ...userState,
+            [name]: value
+        });
+    };
+//WRITE THIS FUNc
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("username is " + userState.email);
+        console.log("username is " + userState.username);
+        console.log("password is " + userState.password);
+    };
+    return (
+        <div>
+            <SignUpForm
+                handleInputChange={handleInputChange}
+                handleFormSubmit = {handleFormSubmit}
+                username = {userState.username}
+                email = {userState.emai}
+                password = {userState.password}
+            >
+            </SignUpForm>
+        </div>
+    )
+
+}
+
+export default Signup;
