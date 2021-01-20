6941c8624cd1ea1816663e6124871b1aa0198d0c
Brandon Piercy <b.piercy567@gmail.com>
2021-01-20 14:32:01 -0800

    centered the splash no login

--- client/src/components/SplashNoLogin/index.js
+++ client/src/components/SplashNoLogin/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {Form, Button} from 'react-bootstrap'; 
 import SearchBar from '../SearchBar';
+import "./style.css";
 
 function SplashNoLogin(){
-    const btnStyle = {"margin": "0 auto", "display":"block", "marginBottom":"10px","flex":"1", "width":"clamp(100px,10%,300px)"};
     return(
         <>
-        <Form.Group style={{"marginTop":"40px"}}>
-            <SearchBar/>
-            <div style={{"display":"flex","flexDirection": "column"}}>
-            <Button variant="light" size="lg" style={btnStyle}>
-                Login
-            </Button>
-            <Button variant="light" size="lg" style={btnStyle}>
-                Sign Up
-            </Button>
+        <Form.Group>
+            <div className="no-login-box">
+                <SearchBar/>
+                <Button variant="light" size="lg">
+                    Login
+                </Button>
+                <Button variant="light" size="lg">
+                    Sign Up
+                </Button>
             </div>
         </Form.Group>
         </> 
