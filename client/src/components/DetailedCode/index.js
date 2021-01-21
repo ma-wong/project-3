e19dca0223bdfaebd405c79bf19e0744c7e72fd7
Morgan <morganw118@gmail.com>
2021-01-20 17:31:12 -0800

    almost finished with the details page skeleton - added description and rating and comments

--- client/src/components/DetailedCode/index.js
+++ client/src/components/DetailedCode/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import "./style.css";
+import "./DetailedCode.css";
 
 function DetailedCode(props) {
     return (
-        <div class="container">
-            <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" value="function hello() alert('Hello');"></textarea>
-                <label for="floatingTextarea">Code Block</label>
+        <div>
+            <div className="card description-card">
+                <div className="card-header">
+                    Author: Morgan
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Title: Cows</h5>
+                    <p className="card-text">Description: Highland cows are the best cows.</p>
+                    <a href="#" className="btn btn-primary">Cow-Lang</a>
+                </div>
+                <div className="card-footer text-muted">
+                    Date Added: 1/20/20
+                </div>
             </div>
         </div>
     )
