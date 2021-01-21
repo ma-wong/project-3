909e3a69b4c20cb8b77813bc36bab6812ee6fccf
Lawson Kelly <lawsonkelly@Lawsons-MacBook-Air.local>
2021-01-20 21:25:17 -0500

    fixed merge issue

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react"
 import "../pageStyles/Details.css";
-import { useHistory } from 'react-router-dom'
-
 
 class Details extends Component {
     constructor(props) {
