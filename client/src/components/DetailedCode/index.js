62ba491101b7e1cb96eaacd44d777e4cc487f2db
Morgan <morganw118@gmail.com>
2021-01-20 23:26:20 -0800

    did some style changes on results and fixed details page

--- client/src/components/DetailedCode/index.js
+++ client/src/components/DetailedCode/index.js
@@ -9,9 +9,9 @@ function DetailedCode(props) {
                     Author: Morgan
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title">Title: Cows</h5>
-                    <p className="card-text">Description: Highland cows are the best cows.</p>
-                    <a href="#" className="btn btn-primary">Cow-Lang</a>
+                    <h5 className="card-title">Title: "Hello World!" in COW language</h5>
+                    <p className="card-text">Description: The COW programming language is an esoteric programming language created by Sean Heber in 2003. It is a Brainfuck variant designed humorously with Bovinae in mind. COW has twelve instructions (four more than Brainfuck) and is Turing-complete. Most instructions are moos, only the capitalization varies: mOo, moO, mOO, Moo, and so on. MMM, OOO, oom and OOM are the exceptions. All other character combinations are ignored and treated as comments.</p>
+                    <a href="#" className="btn btn-primary">COW</a>
                 </div>
                 <div className="card-footer text-muted">
                     Date Added: 1/20/20
--- client/src/components/SearchResultsContainer/index.js
+++ client/src/components/SearchResultsContainer/index.js
@@ -8,28 +8,26 @@ export function SearchResultsList({ children }) {
 
 export function SearchResultsItem(props) {
     return (
-        <li className="list-group-item">
-            <div className="container">
-                <div className="row">
-                    <div className="results-card card mb-3">
-                        <div className="row g-0">
-                            <img className="snippet-img col-md-7" src={Img} />
-                            <div className="col-md-5">
-                                <div className="card-body">
-                                    <h5 className="card-title">Code Snippet Title</h5>
-                                    <p className="card-text">This is a description of the code snippet</p>
-                                    <div className="d-grid gap-2 d-md-block">
-                                        <button className="btn btn-primary" type="button">Tag</button>
-                                        <button className="btn btn-primary" type="button">Another Tag</button>
-                                    </div>
-                                    <br />
-                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <div className="container">
+            <li className="list-group-item">
+                <div className="results-card card">
+                    <div className="row g-0">
+                        <img className="snippet-img col-md-7" src={Img} />
+                        <div className="col-md-5">
+                            <div className="card-body">
+                                <h5 className="card-title">Code Snippet Title</h5>
+                                <p className="card-text">This is a description of the code snippet</p>
+                                <div className="d-grid gap-2 d-md-block">
+                                    <button className="btn btn-primary" type="button">Tag</button>
+                                    <button className="btn btn-primary" type="button">Another Tag</button>
                                 </div>
+                                <br />
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </li>
+            </li>
+        </div>
     )
 }
