7789686adfa12a10674bfc25d7dfa4371c20d3ee
Morgan <morganw118@gmail.com>
2021-01-25 17:48:58 -0800

    added image to splash login and small changes on details

--- client/src/components/SplashLogin/index.js
+++ client/src/components/SplashLogin/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {Container, Button} from 'react-bootstrap';
 import "./SplashLogin.css";
 import { Link } from "react-router-dom";
+import backgroundImg from './ImgTest/test1.png'
 
 function SplashLogin(){
-    // const btnStyle = {"margin": "0 auto", "display":"block", "fontSize":"1.5rem",
-    // "padding":"clamp(15px, 5%, 50px)", "backgroundColor":"rgb(255, 255, 255, .4)", "border":"0", "color":"black"};
 
     return(
-        <div>
-        {/* <div style={{"display":"flex", "justifyContent":"center"}}>
-        <Container   style={{"padding":"0","maxWidth":"800px"}}>
-            <div style={{"margin":"10% auto", "marginTop": "10%", "display":"flex", "flexDirection": "row"}}>
-            <Button style={btnStyle} href="/results">Search</Button>
-            <Button style={btnStyle} href="/browse">Browse</Button>
-            <Button style={btnStyle} href="/create">Create</Button>
-            </div>
-        </Container>
-        </div>   */}
+
+        <div style={{ backgroundImage: `url(${backgroundImg})`}}>
 
             <div className="container btn-container">
                 
