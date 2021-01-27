799555e6743a780e294ce7ae3f022924c290c0cd
JohnWestermeyer <johnwestermeyer@gmail.com>
2021-01-26 21:22:11 -0800

    jordan

--- client/src/components/BrowseFilter/index.js
+++ client/src/components/BrowseFilter/index.js
@@ -5,9 +5,9 @@ import './style.css'
 function BrowseFilter(props){    
     return(
         <div class="sort-button-container">
-        <Button variant="light" size="lg" class="sort-button" onClick={()=>props.changeSortCopies()}>Most Copies</Button>
-        <Button variant="light" size="lg" class="sort-button" onClick={()=>props.changeSortLikes()}>Most Likes</Button>
-        <Button variant="light" size="lg" class="sort-button" onClick={()=>props.changeSortComments()}>Most Comments</Button>
+        <Button variant="light" size="lg" className="sort-button" onClick={()=>props.changeSortCopies()}>Most Copies</Button>
+        <Button variant="light" size="lg" className="sort-button" onClick={()=>props.changeSortLikes()}>Most Likes</Button>
+        <Button variant="light" size="lg" className="sort-button" onClick={()=>props.changeSortComments()}>Most Comments</Button>
         </div>
     )
 };
