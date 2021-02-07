0f5b8fd490ecd09f7ff5aad409f8bcc1b3451dfc
Brandon Piercy <b.piercy567@gmail.com>
2021-02-07 12:42:24 -0800

    changed a tags to link components from react-router-dom, updated nav menu styling, created an animated hamburger menu icon to control the left push menu

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React, { Component} from "react";
 import "./nav.css";
 import API from "../../utils/API";
+import { Link } from "react-router-dom";
 
   class Navhead extends Component {
 
@@ -20,12 +21,12 @@ import API from "../../utils/API";
     }
 
     handleNavMenuClick = (event) => {
-      if (event.target.value === "navigation-menu" && this.state.isNavMenuOpen === false) {
+      if ((event.target.className === "hamburger-button" || event.target.className === "hamburger-button-burger") && this.state.isNavMenuOpen === false) {
         this.setState({
           isNavMenuOpen:true
         });
         document.body.classList.add('nav-menu-push-toright');
-      } else if (event.target.value === "navigation-menu" && this.state.isNavMenuOpen === true) {
+      } else if ((event.target.className === "hamburger-button open" || event.target.className === "hamburger-button-burger") && this.state.isNavMenuOpen === true) {
         this.setState({
           isNavMenuOpen:false
         });
@@ -49,10 +50,10 @@ import API from "../../utils/API";
       <div>
         <nav className={ this.state.isNavMenuOpen ? "nav-menu nav-menu-vertical nav-menu-left nav-menu-open": "nav-menu nav-menu-vertical nav-menu-left"} id="menu-1">
           <h3>Snippet.ltd</h3>
-          <a href="#">Home</a>
-          <a href="#">Search</a>
-          <a href="#">Browse</a>
-          <a href="#">Create</a>
+          <Link to="/">Home</Link>
+          <Link to="/results">Search</Link>
+          <Link to="/browse">Browse</Link>
+          <Link to="/create">Create</Link>
         </nav>
         <nav className={ this.state.isAccountMenuOpen ? "nav-menu nav-menu-vertical nav-menu-right nav-menu-open": "nav-menu nav-menu-vertical nav-menu-right"} id="menu-2">
           <a href="#">Account Settings</a>
@@ -60,7 +61,9 @@ import API from "../../utils/API";
           <a href="#">Help</a>
         </nav>
         <div className="nav-control-row">
-          <button className="hamburger-button" onClick={this.handleNavMenuClick} value="navigation-menu">Left</button>
+          <div className={ this.state.isNavMenuOpen ? "hamburger-button open": "hamburger-button"} onClick={this.handleNavMenuClick} value="navigation-menu">
+            <div className="hamburger-button-burger" value="navigation-menu"></div>
+          </div>
           <button className="account-button" onClick={this.handleNavMenuClick} value="account-menu">Right</button>
         </div>
       </div>
